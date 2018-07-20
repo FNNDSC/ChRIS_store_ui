134cc609d0d697ab8504ef772d91ae95644365e7
jdtzmn <jdtzmn@gmail.com>
2018-07-20 18:41:00 -0400

    Optimized for internet explorer using polyfills and updated CSS  (#11)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/* polyfills */
+import 'core-js/fn/array/includes';
+
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
