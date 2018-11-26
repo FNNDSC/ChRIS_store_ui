d6c96c5122c4961a6068bd69c577b6f2ca22b721
jcaianirh <35978579+jcaianirh@users.noreply.github.com>
2018-11-26 16:09:19 -0500

    Add login logout flow. Add dashboard items. Add license info to plugin details view.

--- src/components/Dashboard/components/DashPluginCardView/DashPluginCardView.js
+++ src/components/Dashboard/components/DashPluginCardView/DashPluginCardView.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as _ from 'lodash-es';
 import {
-  UtilizationCard,
-  UtilizationCardDetails,
-  UtilizationCardDetailsCount,
-  UtilizationCardDetailsDesc,
-  UtilizationCardDetailsLine1,
-  UtilizationCardDetailsLine2,
-  DonutChart,
-  SparklineChart,
   Col,
   CardTitle,
   CardBody,
-  EmptyState,
   EmptyStateAction,
   EmptyStateInfo,
-  EmptyStateTitle,
   Button,
   Card,
+  CardHeading,
+  DropdownKebab,
+  MenuItem,
+  FieldLevelHelp,
 } from 'patternfly-react';
 
 import './DashPluginCardView.css';
 import BrainImg from '../../../../assets/img/empty-brain-xs.png';
 import PluginPointer from '../../../../assets/img/brainy_welcome-pointer.png';
+import RelativeDate from '../../../RelativeDate/RelativeDate';
 
 const DashGitHubEmptyState = () => (
   <Col xs={12}>
@@ -52,91 +48,93 @@ const DashGitHubEmptyState = () => (
     </Card>
   </Col>);
 
+const DashApplicationType = (type) => {
+  if (type === 'ds') {
+    return (<React.Fragment><span className="fa fa-database" /> Data System</React.Fragment>);
+  }
+  return (<React.Fragment><span className="fa fa-file" /> File System</React.Fragment>);
+};
+
 const DashPluginCardView = (props) => {
+  let pluginCardBody;
   const { plugins } = props;
   const showEmptyState = _.isEmpty(plugins);
+  const addNewPlugin = (
+    <Col xs={12} sm={6} md={4}>
+      <Card>
+        <CardBody className="card-view-add-plugin">
+          <div>
+            <img width="77" height="61" src={BrainImg} alt="Add new plugin" />
+          </div>
+          <EmptyStateInfo>
+            Click below to add a new ChRIS plugin
+          </EmptyStateInfo>
+          <EmptyStateAction>
+            <Button bsStyle="primary" bsSize="large" href="/create">
+              Add Plugin
+            </Button>
+          </EmptyStateAction>
+        </CardBody>
+      </Card>
+    </Col>);
+  if (plugins) {
+    pluginCardBody = plugins.map((plugin) => {
+      const creationDate = new RelativeDate(plugin.creation_date);
+      const applicationType = new DashApplicationType(plugin.type);
+      return (
+        <Col xs={12} sm={6} md={4}>
+          <Card>
+            <CardHeading>
+              <CardTitle>
+                <DropdownKebab id="myKebab" pullRight className="card-view-kebob">
+                  <MenuItem>
+                    Delete
+                  </MenuItem>
+                </DropdownKebab>
+                <Link to={`/plugin/${plugin.name}`} href={`/plugin/${plugin.name}`}>{plugin.name}</Link>
+                <div className="card-view-tag-title">
+                  <FieldLevelHelp content={
+                    <div>{plugin.description}</div>}
+                  />
+                </div>
+              </CardTitle>
+            </CardHeading>
+            <CardBody>
+              <div className="card-view-app-type">
+                {applicationType}
+              </div>
+              <div>
+                <div className="card-view-plugin-tag">
+                  {`Version ${plugin.version}`}
+                </div>
+              </div>
+              <div>
+                <div className="card-view-plugin-tag">
+                  {creationDate.isValid() &&
+                  `Created ${creationDate.format()}`
+                  }
+                </div>
+              </div>
+              <div>
+                <div className="card-view-plugin-tag">
+                  {`${plugin.license} license`}
+                </div>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+    });
+    pluginCardBody.push(addNewPlugin);
+  }
+
   return (
     showEmptyState ?
       <DashGitHubEmptyState />
       :
       <React.Fragment>
-        <div className="dashboard-row">
-          <Col xs={12} sm={6} md={4}>
-            <UtilizationCard>
-              <CardTitle>
-                Free Surfer
-              </CardTitle>
-              <CardBody>
-                <strong>Created on </strong>10/5/2018
-                <UtilizationCardDetails>
-                  <UtilizationCardDetailsCount>
-                    900
-                  </UtilizationCardDetailsCount>
-                  <UtilizationCardDetailsDesc>
-                    <UtilizationCardDetailsLine1>
-                      Downloads
-                    </UtilizationCardDetailsLine1>
-                    <UtilizationCardDetailsLine2>
-                      of 1100 Total
-                    </UtilizationCardDetailsLine2>
-                  </UtilizationCardDetailsDesc>
-                </UtilizationCardDetails>
-                <DonutChart
-                  id="donut-chart-1"
-                  data={{
-                    columns: [['Free Surfer', 900], ['Others', 200]],
-                    colors: {
-                      'Free Surfer': '#00659c',
-                      Others: '#393f44',
-                    },
-                  }}
-                  title={{
-                    type: 'total',
-                    secondary: 'Downloads',
-                  }}
-                  tooltip="test tooltip"
-                  legend={{
-                    show: true,
-                    position: 'right',
-                  }}
-                />
-                <SparklineChart
-                  id="line-chart-1"
-                  data={{
-                    columns: [[
-                      'Page Visits',
-                      30,
-                      200,
-                      99,
-                      220]],
-                    type: 'area',
-                  }}
-                />
-              </CardBody>
-            </UtilizationCard>
-          </Col>
-          <Col xs={12} sm={6} md={4}>
-            <UtilizationCard>
-              <CardBody>
-                <EmptyState>
-                  <div>
-                    <img width="77" height="61" src={BrainImg} alt="Add new plugin" />
-                  </div>
-                  <EmptyStateTitle>
-                    Add a Plugin
-                  </EmptyStateTitle>
-                  <EmptyStateInfo>
-                    Click below to add a new ChRIS plugin
-                  </EmptyStateInfo>
-                  <EmptyStateAction>
-                    <Button bsStyle="primary" bsSize="large" href="/create">
-                      Add Plugin
-                    </Button>
-                  </EmptyStateAction>
-                </EmptyState>
-              </CardBody>
-            </UtilizationCard>
-          </Col>
+        <div className="card-view-row">
+          {pluginCardBody}
         </div>
       </React.Fragment>
   );
