c9a3539ab8a445c517b3540711a824dec4c8e65d
Gagan Kumar <gaganskumar3@gmail.com>
2019-04-17 15:19:10 -0400

    Fixing test

--- src/components/Router/Router.test.js
+++ src/components/Router/Router.test.js
@@ -24,8 +24,8 @@ describe('Router', () => {
     expect(wrapper.find('Switch')).toHaveLength(1);
   });
 
-  it('should render 7 Routes', () => {
-    expect(wrapper.find('Route')).toHaveLength(7);
+  it('should render 5 Routes', () => {
+    expect(wrapper.find('Route')).toHaveLength(5);
   });
 
   it('every route should have a path prop and component prop', () => {
