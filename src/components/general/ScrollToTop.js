8c7d3f8833bff531b069104b7769ebac6d3468f9
Divyanshi Singh <divyanshipilot@gmail.com>
2021-03-31 15:40:16 +0530

    Page scroll restoration implemented(#14)

---
+++ src/components/general/ScrollToTop.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+class ScrollToTop extends React.Component {
+  componentDidUpdate(prevProps) {
+      console.log("done");
+    if (
+      this.props.location.pathname !== prevProps.location.pathname
+    ) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default withRouter(ScrollToTop);
