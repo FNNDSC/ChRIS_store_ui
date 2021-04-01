1404e31ff55820b847133af4d9d546e789248f00
Divyanshi Singh <divyanshipilot@gmail.com>
2021-04-01 12:08:38 +0530

    callToAction content and button added

--- src/components/Welcome/components/WelcomeDevelopers/WelcomeDevelopers.js
+++ src/components/Welcome/components/WelcomeDevelopers/WelcomeDevelopers.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import './WelcomeDevelopers.css';
+import React from "react";
+import "./WelcomeDevelopers.css";
+import { Link } from "react-router-dom";
 
 const WelcomeDevelopers = () => (
-  <div className="welcome-developers row">
-    <div className="welcome-developers-header">Developers Call To Action.</div>
-  </div>
+  <React.Fragment>
+    <div className="welcome-developers column">
+      <div className="welcome-developers-header">
+        Expand the reach of your image processing software
+      </div>
+      <div className="text-light">
+        <p>
+          ChRIS is an <strong>open source platform</strong> for medical
+          analytics in the cloud, democratizing the development of image
+          processing apps within an ecosystem following
+          <strong> common standards, rather than disparate silos</strong>.
+        </p>
+        <p>
+          A ChRIS Developer account enables you to submit your image processing
+          application as a containerized ChRIS plugin and share it with the
+          broader ChRIS community of researchers and clinicians.{" "}
+          <strong>Join us!</strong>
+        </p>
+      </div>
+      <Link to="/quickstart" className="btn callToAction-btn btn-primary">
+        Learn More
+      </Link>
+    </div>
+  </React.Fragment>
 );
 
 export default WelcomeDevelopers;
--- src/components/Welcome/components/WelcomeDevelopers/WelcomeDevelopers.test.js
+++ src/components/Welcome/components/WelcomeDevelopers/WelcomeDevelopers.test.js
@@ -12,20 +12,20 @@ describe('WelcomeDevelopers', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should render row div', () => {
-    expect(wrapper.find('div.row')).toHaveLength(1);
+  it('should render column div', () => {
+    expect(wrapper.find('div.column')).toHaveLength(1);
   });
 
-  it('should render the row div with class welcome-developers', () => {
+  it('should render the column div with class welcome-developers', () => {
     expect(wrapper
-      .find('div.row')
+      .find('div.column')
       .hasClass('welcome-developers'))
       .toEqual(true);
   });
 
   it('should render the div with class welcome-developers-header', () => {
     expect(wrapper
-      .find('div.row')
+      .find('div.column')
       .find('div.welcome-developers-header').length)
       .toEqual(1);
   });
