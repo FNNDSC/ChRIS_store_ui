b9dc195aa895f6efdb0e12e0fb1256d2aeecf5c8
Shipra Verma <vaaishpra19@gmail.com>
2021-04-06 17:27:18 +0530

    prerender routes with snap

--- src/index.js
+++ src/index.js
@@ -8,5 +8,11 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
 registerServiceWorker();
