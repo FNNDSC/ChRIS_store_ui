548d57ce5c5f432c47c55a2e19eceaf578e5f557
Divyanshi Singh <divyanshipilot@gmail.com>
2021-04-15 12:32:55 +0530

    button responsiveness on small screens and code refactor

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Button } from '@patternfly/react-core';
-import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
-const ButtonComponent = (props) => {
-  const {
-    variant,
-    onClick,
-    customClass,
-    loading,
-    toRoute,
-    children,
-    ...otherProps
-  } = props;
+import React from "react";
+import { Button } from "@patternfly/react-core";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
+import "./button.css";
+const ButtonComponent = ({
+  variant,
+  onClick,
+  customClass,
+  loading,
+  toRoute,
+  children,
+  ...otherProps
+}) => {
   const history = useHistory();
-  return(
+  return (
     <div className="chris-button">
       <Button
         isLoading={loading}
         spinnerAriaValueText
         variant={variant}
-        onClick={toRoute?history.push(toRoute):onClick}
+        onClick={toRoute ? history.push(toRoute) : onClick}
         className={customClass}
         {...otherProps}
       >
         {children}
-        </Button>
+      </Button>
     </div>
   );
 };
