e5a29d9b9cc8ed56f35e7d6f176a6b32cc6cd5fe
Alisamar Husain <zrthxn@gmail.com>
2021-04-19 15:25:26 +0530

    Temporary support for reStructuredText files

--- src/components/Plugin/components/PluginBody/PluginBody.js
+++ src/components/Plugin/components/PluginBody/PluginBody.js
@@ -2,8 +2,8 @@
 /* eslint-disable react/jsx-indent */
 import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import marked from 'marked';
+import rst2html from 'rst2html';
 import {
   Grid, Nav, NavItem, TabContainer, TabContent, TabPane,
 } from 'patternfly-react';
@@ -15,23 +15,52 @@ const PluginBody = ({ pluginData }) => {
   const [readme, setReadme] = useState();
   const [repoData, setRepoData] = useState();
 
+  const getReadmeFileType = (url) => (
+    url
+      .split('').reverse().join('')   // Reverse the URL
+      .split('.').shift()             // Get first segment before '.'
+      .split('').reverse().join('')   // Reverse again to get original
+  )
+
   const fetchReadme = useCallback(async (repo) => {
-    const data = await fetch(`https://api.github.com/repos/${repo}/community/profile`)
-    const rm = await fetch((await data.json()).files.readme.url)
-    const download = await fetch((await rm.json()).download_url)
-    setReadme(await download.text())
+    const rurl = await fetch((
+      await (
+        await fetch(`https://api.github.com/repos/${repo}/community/profile`)
+      ).json()).files.readme.url)
+
+    const downloadUrl = (await rurl.json()).download_url
+    const readmeType = getReadmeFileType(downloadUrl)
+    const file = await fetch(downloadUrl)
+
+    if (readmeType === 'md' || readmeType === 'rst') // temporary until we find a way to render rst
+      setReadme(marked(await file.text()))
+    // else if (readmeType === 'rst')
+    //   setReadme(rst2html(await file.text()))
+    else if (readmeType === 'html' || readmeType === 'htm')
+      setReadme(await file.text())
+    else 
+      throw TypeError('Unknown README file type:', readmeType)
   }, [])
 
   const fetchRepoData = useCallback(async (repo) => {
-    const data = await fetch(`https://api.github.com/repos/${repo}`)
-    setRepoData(await data.json())
-    fetchReadme(repo)
+    try {
+      const data = await fetch(`https://api.github.com/repos/${repo}`)
+      setRepoData(await data.json())
+      fetchReadme(repo)
+    } catch (error) {
+      console.error(error)
+      throw Error(error)
+    }
   }, [fetchReadme])
 
   useEffect(() => {
     const expectRepoName = pluginData.public_repo.split('github.com/')[1];
     if (expectRepoName)
-      fetchRepoData(expectRepoName)
+      try {
+        fetchRepoData(expectRepoName)
+      } catch (error) {
+        console.error(error)
+      }
     else
       return () => {
         setRepoData(undefined)
@@ -66,24 +95,24 @@ const PluginBody = ({ pluginData }) => {
                               <div className="plugin-body-readme">
                                 README
                               </div>
-                              { readme ? <div dangerouslySetInnerHTML={{ __html: marked(readme) }}></div> : null }
+                              { readme ? <div dangerouslySetInnerHTML={{ __html: readme }}></div> : null }
                             </div>
                             <div className="plugin-body-side-col">
                               <div className="plugin-body-copy-url">
                                 <CopyURLButton className="pf-c-button pf-m-primary" text={pluginData.url} />
                               </div>
                               <div className="plugin-body-detail-section">
-                                <h4>Public Repo</h4>
+                                <h4>Repository</h4>
                                 <a href={pluginData.public_repo}>
                                   {pluginData.public_repo}
                                 </a>
                               </div>
-                              <div className="plugin-body-detail-section">
+                              {/* <div className="plugin-body-detail-section">
                                 <h4>Documentation</h4>
                                 <a className="pf-c-button pf-m-link" href="readthedocs.com/freesurfer">
                                   readthedocs.com/freesurfer
                                 </a>
-                              </div>
+                              </div> */}
                               <div className="plugin-body-detail-section">
                                 <h4>Contributors</h4>
                                 <a href={pluginData.authorURL}
