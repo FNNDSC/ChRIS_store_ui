4bf8888758224a0128f45dade8eefe2be69d40d7
sonylomo <sonylomo1@gmail.com>
2021-04-21 06:05:16 +0000

    wrapper fix

--- src/components/Welcome/components/WelcomeCategory/WelcomeCategory.js
+++ src/components/Welcome/components/WelcomeCategory/WelcomeCategory.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from "react";
 import { Card, CardTitle, CardBody, CardFooter } from "@patternfly/react-core";
-import Button from '../../../Button';
-import PropTypes from 'prop-types';
-import './WelcomeCategory.css';
+import Button from "../../../Button";
+import PropTypes from "prop-types";
+import "./WelcomeCategory.css";
 
 const WelcomeCategory = ({ category, ...props }) => (
-  <Card className="welcome-category">
-    <CardTitle className="welcome-category-header">{category.name}</CardTitle>
-    <CardBody>
-      {category.items.map(({ name, desc, img, tags }) => (
-        <div className="welcome-category-item" key={name}>
-          <img src={img} alt="" className="welcome-category-item-img" />
-          <div className="welcome-category-item-body">
-            <div className="welcome-category-item-name">{name}</div>
-            <div className="welcome-category-item-desc">{desc}</div>
-            <div className="welcome-category-item-tags">{tags.join(" ")}</div>
+  <div {...props}>
+    <Card className="welcome-category">
+      <CardTitle className="welcome-category-header">{category.name}</CardTitle>
+      <CardBody>
+        {category.items.map(({ name, desc, img, tags }) => (
+          <div className="welcome-category-item" key={name}>
+            <img src={img} alt="" className="welcome-category-item-img" />
+            <div className="welcome-category-item-body">
+              <div className="welcome-category-item-name">{name}</div>
+              <div className="welcome-category-item-desc">{desc}</div>
+              <div className="welcome-category-item-tags">{tags.join(" ")}</div>
+            </div>
           </div>
-        </div>
-      ))}
-    </CardBody>
-    <CardFooter>
-      <Button 
-        variant="primary"
-        className="btn-block"
-        >
+        ))}
+      </CardBody>
+      <CardFooter>
+        <Button variant="primary" className="btn-block">
           View More ...
-      </Button>
-    </CardFooter>
-  </Card>
+        </Button>
+      </CardFooter>
+    </Card>
+  </div>
 );
 
 WelcomeCategory.propTypes = {
--- src/components/Welcome/components/WelcomeFeature/WelcomeFeature.js
+++ src/components/Welcome/components/WelcomeFeature/WelcomeFeature.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './WelcomeFeature.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./WelcomeFeature.css";
 
-const WelcomeFeature = ({img, name, ...props}) => (
-  <div className="welcome-feature">
-    <img src={img} alt={name} className="welcome-feature-img" />
-    <div className="welcome-feature-text">
-      {name}
+const WelcomeFeature = ({ img, name, ...props }) => (
+  <div {...props}>
+    <div className="welcome-feature">
+      <img src={img} alt={name} className="welcome-feature-img" />
+      <div className="welcome-feature-text">{name}</div>
     </div>
   </div>
 );
