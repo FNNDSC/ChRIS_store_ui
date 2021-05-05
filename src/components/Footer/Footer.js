360a5ed1da3cb9515461c139f20179eb7879f6dd
somya51p <somya2001p@gmail.com>
2021-05-05 22:41:57 +0530

    Conflicts resolved

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,42 @@
 import React from "react";
-import styles from "./Footer.module.css";
+import "./Footer.css";
 import chrisLogo from "../../assets/img/chris_logo-white.png";
+
 const Footer = () => (
   <div className="footer">
     <div className="footer-row">
       <div className="footer-logo">
         <img src={chrisLogo} alt="ChRIS Plugin Store" />
       </div>
-      <div className={styles['footer-body']}>
-        <div className={styles['footer-desc']}>
+      <div className="footer-body">
+        <div className="footer-desc">
           ChRIS is developed by Boston Children{"'"}s Hospital in partnership
           with Red Hat, the Massachusetts Open Cloud (MOC), and Boston
           University.
         </div>
-        <div className={styles['footer-links']}>
-          <div className={styles['footer-link']}>
+        <div className="footer-links">
+          <div className="footer-link">
             <a href="https://chrisproject.org">About ChRIS</a>
           </div>
-          <div className={styles['footer-link']}>
+          <div className="footer-link">
             <a href="/quickstart">Submit your plugin</a>
           </div>
-          <div className={styles['footer-link']}>
+          <div className="footer-link">
             <a href="mailto:dev@babyMRI.org?subject=Contact from ChRIS Store">
               Contact Us
             </a>
           </div>
-          <div className={styles['footer-link']}>
+          <div className="footer-link">
             <a href="https://github.com/FNNDSC/ChRIS_store_ui/issues/new">
               Report an Issue
             </a>
           </div>
-           <div className={styles['footer-link']}>
+           <div className="footer-link">
             <a href="https://github.com/FNNDSC/ChRIS_store_ui">
               Contribute
             </a>
           </div>
-          <div className={styles['footer-link']}>
+          <div className="footer-link">
             <a href="mailto:dev@babyMRI.org?subject=ChRIS Store Plugin Request">
               Request a plugin
             </a>
@@ -46,7 +47,7 @@ const Footer = () => (
         </div>
       </div>
     </div>
-    <div className={styles['footer-copyright']}>
+    <div className="footer-copyright">
       © 2018 - {new Date().getFullYear()} Boston Children{"'"}s Hospital, Red
       Hat, Massachusetts Open Cloud, Boston University. All rights reserved.
     </div>
--- src/components/Welcome/components/WelcomeDevelopers/WelcomeDevelopers.js
+++ src/components/Welcome/components/WelcomeDevelopers/WelcomeDevelopers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './WelcomeDevelopers.module.css';
+import { Link } from "react-router-dom";
 
 const WelcomeDevelopers = () => (
   <React.Fragment>
