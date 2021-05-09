1476ab7963164d92f84c7c0aa9cac31b9d835f66
EUGINELETHAL <ochungeugine@gmail.com>
2021-05-09 07:43:49 +0300

    fixed bug setcategories never used

--- src/components/Welcome/components/WelcomeCategories/WelcomeCategories.js
+++ src/components/Welcome/components/WelcomeCategories/WelcomeCategories.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import sampleCategories from "./sampleCategories";
 import "./welcomeCategories.css";
 import WelcomeCategory from "../WelcomeCategory/WelcomeCategory";
