5b833d4735bd3f34e8cd582ba2f6e2682e1e04ad
EUGINELETHAL <ochungeugine@gmail.com>
2021-11-08 01:24:09 +0300

    added collaborators

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -78,7 +78,12 @@ export class SignIn extends Component {
   hideError() {
     this.setState({ error: null });
   }
+<<<<<<< Updated upstream
 
+=======
+  
+  
+>>>>>>> Stashed changes
   render() {
     const {
       error, username, password, loading,
