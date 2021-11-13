2d51157aee75dc4fc6c310ac36a2f4ffed11440a
EUGINELETHAL <ochungeugine@gmail.com>
2021-11-13 13:12:41 +0300

    created dropdown linkfor managing collaborators

--- src/components/Dashboard/components/DashPluginCardView/DashPluginCardView.jsx
+++ src/components/Dashboard/components/DashPluginCardView/DashPluginCardView.jsx
@@ -6,8 +6,7 @@ import {
   Card, 
   CardHeader, 
   CardTitle, 
-  CardBody,
-  CardFooter, 
+  CardBody, 
   CardActions,
   DropdownItem, 
   Dropdown, 
@@ -17,7 +16,6 @@ import {
   Form, 
   Modal 
 } from "@patternfly/react-core";
-import { PlusCircleIcon } from '@patternfly/react-icons';
 import Button from "../../../Button";
 import "./DashPluginCardView.css";
 import BrainImg from "../../../../assets/img/empty-brain-xs.png";
@@ -157,12 +155,14 @@ class DashPluginCardView extends Component {
                   isPlain
                   dropdownItems={[
                     <DropdownItem key={`edit-${plugin.id}`} id={`edit-${plugin.name}`}>Edit</DropdownItem>,
-                    <DropdownItem key={`delete-${plugin.id}`} id={`delete-${plugin.name}`}>Delete</DropdownItem>
+                    <DropdownItem key={`delete-${plugin.id}`} id={`delete-${plugin.name}`}>Delete</DropdownItem>,
+                   <DropdownItem key={`/manage/collaborators/${plugin.name}`} component={<Link to={`/manage/collaborators/${plugin.name}`}>Manage Collaborators</Link>} />
+                    
+                    
                   ]}
                 />
               </CardActions>
             </CardHeader>
-
             <CardBody>
               <h3>{plugin.title}</h3>
               <div className="card-info-points">
@@ -172,17 +172,6 @@ class DashPluginCardView extends Component {
               
               <div className="card-view-app-type">{applicationType}</div>
             </CardBody>
-              <CardFooter
-              className="card-footer"
-            >
-              <CardActions>
-                <Button variant="secondary">
-                  <PlusCircleIcon type="pf" style={{ margin: '0 1em 0 0' }} />
-                  <Link to={`/manage/collaborators/${plugin.name}`}><span>Manage Collaborators</span></Link>
-                </Button>
-              </CardActions>
-
-            </CardFooter>
           </Card>
         </GridItem>
       );
