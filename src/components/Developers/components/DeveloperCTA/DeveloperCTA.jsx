c416c84a30b1aef52547e4972d9ca19d55f2781b
Jennings Zhang <jenni_zh@protonmail.com>
2022-01-14 17:18:04 -0500

    Fix footer placement on /quickstart

--- src/components/Developers/components/DeveloperCTA/DeveloperCTA.jsx
+++ src/components/Developers/components/DeveloperCTA/DeveloperCTA.jsx
@@ -8,35 +8,38 @@ import ConnectedDeveloperSignup from '../DeveloperSignup/DeveloperSignup';
 import ChrisStore from '../../../../store/ChrisStore';
 
 export const DeveloperCTA = ({ store }) => (
-  <Bullseye id="developer-cta">
-    <Grid style={{ maxWidth: '1200px' }} hasGutter>
-      <GridItem lg={6} xs={12}>
-        <div id="developer-cta-header">
-          <h2>Expand the reach of your image processing software</h2>
-          <p style={{ fontSize: 'medium' }}>
-            A ChRIS Developer account enables you to submit your image
-            processing application as a containerized ChRIS plugin and
-            share it with the broader ChRIS community of researchers and
-            clinicians.
-          </p>
-        </div>
-      </GridItem>
-      <GridItem lg={1} xs={12}/>
-      <GridItem lg={5} xs={12}>
-        {
-          store.get('isLoggedIn') ? null :
-            <div id="developer-cta-form">
-              <Card>
-                <CardBody>
-                  <h2 style={{ margin: '0.25em 0 1.5em 0' }}><b>Create a ChRIS Developer Account</b></h2>
-                  <ConnectedDeveloperSignup />
-                </CardBody>
-              </Card>
-            </div>
-        }
-      </GridItem>
-    </Grid>
-  </Bullseye>
+  // FIXME wrap in div so that footer is not misplaced
+  <div>
+    <Bullseye id="developer-cta">
+      <Grid style={{ maxWidth: '1200px' }} hasGutter>
+        <GridItem lg={6} xs={12}>
+          <div id="developer-cta-header">
+            <h2>Expand the reach of your image processing software</h2>
+            <p style={{ fontSize: 'medium' }}>
+              A ChRIS Developer account enables you to submit your image
+              processing application as a containerized ChRIS plugin and
+              share it with the broader ChRIS community of researchers and
+              clinicians.
+            </p>
+          </div>
+        </GridItem>
+        <GridItem lg={1} xs={12}/>
+        <GridItem lg={5} xs={12}>
+          {
+            store.get('isLoggedIn') ? null :
+              <div id="developer-cta-form">
+                <Card>
+                  <CardBody>
+                    <h2 style={{ margin: '0.25em 0 1.5em 0' }}><b>Create a ChRIS Developer Account</b></h2>
+                    <ConnectedDeveloperSignup />
+                  </CardBody>
+                </Card>
+              </div>
+          }
+        </GridItem>
+      </Grid>
+    </Bullseye>
+  </div>
 );
 
 export default ChrisStore.withStore(DeveloperCTA);
