00de1836918fbfba784f781c3929c213e23228ac
Thessa Kranendonk <thessakranendonk@gmail.com>
2022-11-13 17:56:50 -0500

    Fixed bug in Notication.jsx, where Alert was not closing on x-icon click

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
 import PropTypes from 'prop-types';
 import './Notifications.css';
 
-export const Notification = ({ position, variant, message, onClose, closeable, title, timeout }) => (
+export const Notification = ({ position, variant, message, onClose, closeable, title, timeout }) => {
+  const [alertVisible, setAlertVisible] = useState(true)
+
+  const toggleAlert = () => setAlertVisible(false)
+
+  return (
   <div className={`notification ${position}`}>
+    {alertVisible && (
     <Alert
       variant={variant}
       title={title || null}
-      actionClose={closeable && <AlertActionCloseButton onClose={onClose} />}
-      timeout={timeout || 4000}
+      actionClose={closeable && <AlertActionCloseButton onClose={() => toggleAlert()} />}
+      timeout={timeout}
       onTimeout={onClose}
     >
       {message}
     </Alert>
+    )}
   </div>
-);
+)};
 
 Notification.propTypes = {
   position: PropTypes.string.isRequired,
